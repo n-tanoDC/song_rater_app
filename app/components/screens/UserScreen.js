77a228e19c4e67672ce2a6b6399a66f9d283f156
n-tanoDC <nicolas.tano42@gmail.com>
2020-09-30 10:50:35 +0200

    Add conditionnal render depending on userContext

--- app/components/screens/UserScreen.js
+++ app/components/screens/UserScreen.js
@@ -1,14 +1,39 @@
-import { Text, Button } from 'native-base';
+import { useIsFocused } from '@react-navigation/native';
+import { Text, Button, Container, ListItem, List } from 'native-base';
 import React, { useContext, useState } from 'react';
 import { UserContext } from '../../App';
-import Login from '../modals/LoginModal'
-import Signup from '../modals/SignupModal'
+import Login from '../auth/LoginModal';
+import Signup from '../auth/SignupModal';
 
 export default () => {
   const [login, switchLogin] = useState(false)
   const [signup, switchSignup] = useState(false)
 
   const userContext = useContext(UserContext)
+
+  if (userContext.user) {
+    const { user } = userContext
+
+    console.log(user);
+
+    let favs;
+
+    if (user.favorites.length > 0) {
+      favs = user.favorites.map(fav => <ListItem key={fav}><Text>{fav}</Text></ListItem>)
+    }
+
+    return (
+      <Container>
+        <Text>Nom d'utilisateur : {user.username}</Text>
+        <Text>Email : {user.email}</Text>
+        <Text>Description : {user.description ? user.description : 'Aucune description.'}</Text>
+        <Text>Artistes favoris :</Text>
+        <List>
+          {favs}
+        </List>
+      </Container>
+    )
+  }
 
   return (
     <>
