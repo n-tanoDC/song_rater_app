b419d7e3af6b4d60788a2b6b2f6433ed0ff2692e
n-tanoDC <nicolas.tano42@gmail.com>
2020-09-30 10:48:55 +0200

    Create the remaining main screens/tabs

--- app/Navigator.js
+++ app/Navigator.js
@@ -6,16 +6,17 @@ import Home from './components/screens/HomeScreen'
 import User from './components/screens/UserScreen'
 import Feed from './components/screens/FeedScreen'
 import Music from './components/screens/MusicScreen'
-
+import Search from './components/screens/SearchScreen'
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export default () => 
   <NavigationContainer>
-    <Navigator headerMode="none" initialRouteName="Home">
-      <Screen name="Home" component={Home} />
-      <Screen name="User" component={User} />
-      <Screen name="Feed" component={Feed} />
+    <Navigator lazy={false} headerMode="none" initialRouteName="Home">
+      <Screen name="Search" component={Search} />
       <Screen name="Music" component={Music} />
+      <Screen name="Home" component={Home} />
+      <Screen name="Feed" component={Feed} />
+      <Screen name="User" component={User} />
     </Navigator>
   </NavigationContainer>
