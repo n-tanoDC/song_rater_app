22c47d054c860037b6018d00dbd5ceb3e31892d0
n-tanoDC <nicolas.tano42@gmail.com>
2020-10-12 16:51:32 +0200

    Givea fixed size to component

--- app/components/common/ButtonIcon.js
+++ app/components/common/ButtonIcon.js
@@ -3,5 +3,6 @@ import React from 'react';
 
 export default ({ name, color, onPress }) => 
   <Button transparent onPress={() => onPress()}>
-    <Icon style={{ color: color }} name={name} />
+    <Icon style={{ color: color, fontSize: 28 }} name={name} />
   </Button>
+
