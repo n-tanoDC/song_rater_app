6b25ef2ea1e91a30c34e25aaa74cd246995187ad
n-tanoDC <nicolas.tano42@gmail.com>
2020-10-21 08:59:41 +0200

    Add function to calculate average rating of a media

--- app/functions.js
+++ app/functions.js
@@ -1,8 +1,22 @@
 import { ToastAndroid } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 
+
+// return formatted Array of artists, used to send post request to the API
+export const getFormattedArtists = artists => {
+  return artists.map(artist => ({ id: artist.id, name: artist.name }))
+}
+
+
+// display a toast at the bottom of the screen
+export const showToast = message => {
+  ToastAndroid.show(message, ToastAndroid.SHORT)
+}
+
+
 // return formatted string of artists from an Array (provided in the Spotify API response object)
-export const getArtists = artists => {
+export const getArtists = media => {
+  const { artists } = media;
   let artistsNames = '';
   for (const [index, artist] of artists.entries()) {
     const suffix = index < artists.length - 1 ? ', ' : '';
@@ -11,30 +25,46 @@ export const getArtists = artists => {
   return artistsNames;
 }
 
-// return formatted Array of artists, used to send post request to the API
-export const getFormattedArtists = artists => {
-  return artists.map(artist => ({ id: artist.id, name: artist.name }))
-}
 
-// display a toast at the bottom of the screen
-export const showToast = message => {
-  ToastAndroid.show(message, ToastAndroid.SHORT)
-}
-
-// return a string corresponding to the right url to use to display an element cover
-export const getCover = element => {
-  if (element.image) {
-    return element.image;
+// return a string corresponding to the right url to use to display a media cover
+export const getCover = media => {
+  if (media.image) {
+    return media.image;
   } 
-  return element.type === 'track' ? element.album.images[0].url : element.images[0].url;
+  return media.type === 'track' ? media.album.images[0].url : media.images[0].url;
 }
 
-export const isVisiting = (user, author) => {
-  if (user) {
-    return user.username !== author.username;
+
+export const getLink = media => {
+  if (media.link) {
+    return media.link;
+  }
+  return media.external_urls.spotify;
+}
+
+
+export const getAverageRating = reviews => {
+  if (!reviews || reviews.length === []) {
+    return null;
+  }
+  const ratings = reviews.map(review => review.rating);
+  const sum = ratings.reduce((a, b) => a + b, 0);
+  return Math.round((sum / reviews.length) * 100) / 100;
+}
+
+
+export const isFollowing = (connectedUser, userToTest) => {
+  return connectedUser.following.some(userFollowed => userFollowed === userToTest._id)
+}
+
+
+export const isVisiting = (connectedUser, author) => {
+  if (connectedUser) {
+    return connectedUser.username !== author.username;
   }
   return true;
 }
+
 
 export const pickImage = (callback) => {
   ImagePicker.showImagePicker({
@@ -55,10 +85,3 @@ export const pickImage = (callback) => {
     }
   })
 }
-
-
-export const isFollowing = (connectedUser, userToTest) => {
-  return connectedUser.following.some(userFollowed => userFollowed === userToTest._id)
-}
-
-
