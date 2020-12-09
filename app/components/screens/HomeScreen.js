c716619b63bf3cbcf4c73e91c05c3f17bbd575e9
n-tanoDC <nicolas.tano42@gmail.com>
2020-12-09 09:10:53 +0100

    Adapt HomeScreen to TabView component

--- app/components/screens/HomeScreen.js
+++ app/components/screens/HomeScreen.js
@@ -8,41 +8,55 @@ import { getAllFollowingReviews, getAllReviews } from '../../data/reviews';
 
 import { UserContext } from '../../contexts/UserContext';
 
+const getReviewsList = (params) => (
+  <ReviewsList 
+    object={params.object}
+    reviews={params.reviews}
+    setReviews={params.setReviews}
+    getReviews={params.getReviews} />
+)
+
+const getSections = (params) => ([
+  { 
+    title: 'Toutes les critiques',
+    render: () => getReviewsList({
+      reviews: params.allReviews,
+      setReviews: params.setAllReviews,
+      getReviews: params.getAllReviews
+    }),
+  },
+  { 
+    title: 'Abonnements',
+    render: () => getReviewsList({
+      reviews: params.subscriptionsReviews,
+      setReviews: params.setSubscriptionsReviews,
+      getReviews: params.getAllFollowingReviews,
+      object: params.connectedUser
+    }),
+  }
+])
+
 export default () => {
   const [allReviews, setAllReviews] = useState(null);
   const [subscriptionsReviews, setSubscriptionsReviews] = useState(null);
 
   const { connectedUser } = useContext(UserContext);
 
-  const allReviewsList = (
-    <ReviewsList 
-      reviews={allReviews}
-      setReviews={setAllReviews}
-      getReviews={getAllReviews} />
-  )
-  
-  const subscriptionsReviewsList = (
-      <ReviewsList
-        object={connectedUser}
-        reviews={subscriptionsReviews}
-        setReviews={setSubscriptionsReviews}
-        getReviews={getAllFollowingReviews} />
-    )
+  const tabViewParams = {
+    allReviews, setAllReviews, getAllReviews,
+    subscriptionsReviews, setSubscriptionsReviews, getAllFollowingReviews,
+    connectedUser
+  }
 
-  const sections = [
-    { 
-      title: 'Toutes les critiques',
-      render: () => allReviewsList,
-    },
-    { 
-      title: 'Abonnements',
-      render: () => subscriptionsReviewsList,
-    }
-  ]
+  const allReviewsList = getReviewsList({
+    reviews: allReviews,
+    setReviews: setAllReviews,
+    getReviews: getAllReviews
+  })
 
   return (
     <SafeAreaView style={styles.container}>
-      {connectedUser ? <CustomTabView style='rounded' sections={sections} /> : allReviewsList}
+      {connectedUser ? <CustomTabView style='rounded' sections={getSections(tabViewParams)} /> : allReviewsList}
     </SafeAreaView>
   )
 };
