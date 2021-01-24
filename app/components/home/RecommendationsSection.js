96e8ab847837abea10a3868bd48e9b928027b1fa
n-tanoDC <nicolas.tano42@gmail.com>
2021-01-24 12:18:55 +0100

    Improve RecommendationsSection behavior when no user is connected

--- app/components/home/RecommendationsSection.js
+++ app/components/home/RecommendationsSection.js
@@ -1,20 +1,25 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import CustomButton from '../common/buttons/CustomButton';
 import Loader from '../common/Loader';
+import SectionTitle from '../common/SectionTitle';
+import AlbumsList from '../media/albums/AlbumsList';
+
+import { getRecommendations } from '../../functions/spotify';
+import { catchErrors } from '../../functions/errors';
 
 import { AppContext } from '../../contexts/AppContext';
 import { UserContext } from '../../contexts/UserContext';
-import { getRecommendations } from '../../functions/spotify';
-import { catchErrors } from '../../functions/errors';
-import AlbumsList from '../media/albums/AlbumsList';
-import SectionTitle from '../common/SectionTitle';
 
 export default () => {
   const [medias, setMedias] = useState(null)
   const { token } = useContext(AppContext)
   const { connectedUser } = useContext(UserContext)
-
-  useEffect(() => { 
+  const navigation = useNavigation()
+  
+  const getRecommendationsData = () => {
     if (connectedUser) {
       if (connectedUser.favorites.length) {
         getRecommendations(token, connectedUser)
@@ -27,17 +32,42 @@ export default () => {
       } else {
         setMedias([])
       }
+    } else {
+      setMedias([])
     }
-  }, [])
+  }
+  
+  useEffect(() => { getRecommendationsData() }, [])
+
+  let listComponent = (
+    <View style={styles.listContainer}>
+      <AlbumsList showArtists albums={medias}/>
+    </View>
+  )
+
+  if (!connectedUser) {
+    listComponent = (
+      <View style={styles.emptyListContainer}>
+        <Text style={styles.emptyListLabel}>Veuillez-vous connecter ou créer un compte pour voir les recommandations.</Text>
+        <CustomButton 
+          text={'Se connecter'}
+          onPress={() => navigation.navigate('Account')} />
+      </View> 
+    )
+  } else {
+    listComponent = (
+      <View style={styles.listContainer}>
+        <AlbumsList showArtists albums={medias}/>
+      </View>
+    )
+  }
 
   if (!medias) return <Loader />
 
   return (
     <View style={styles.container}>
-      <SectionTitle text='Titres recommandés' />
-      <View style={styles.listContainer}>
-        <AlbumsList showArtists albums={medias}/>
-      </View>
+      <SectionTitle icon='reload' onPress={() => getRecommendationsData()} text='Titres recommandés' />
+      {listComponent}
     </View>
   )
 }
@@ -48,5 +78,12 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     minHeight: 100,
+  },
+  emptyListContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyListLabel: {
+    marginBottom: 10
   }
 })
