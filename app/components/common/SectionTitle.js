eb0cc9093cc2252bd98475f91daf38fcdb086ad0
n-tanoDC <nicolas.tano42@gmail.com>
2021-01-24 12:16:25 +0100

    Add icon and onPress to SectionTitle

--- app/components/common/SectionTitle.js
+++ app/components/common/SectionTitle.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from '../../styles/colors';
 
-export default ({ text }) => {
+export default ({ text, icon, onPress }) => {
   return (
-    <Text style={styles.sectionTitle}>{text}</Text>
+    <View style={styles.container}>
+      <Text style={styles.sectionTitle}>{text}</Text>
+      {icon ? (
+        <TouchableOpacity onPress={onPress}>
+          <Icon name={icon} color={colors.darkgrey} size={22} />
+        </TouchableOpacity>) : null}
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
   sectionTitle: {
     fontSize: 22,
-    marginVertical: 20,
     fontFamily: 'baloo2-semibold',
     color: colors.darkgrey,
   }
