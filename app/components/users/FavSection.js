66fd29c9d6ccc901d1ba5235270c49e5fa2ccaf5
n-tanoDC <nicolas.tano42@gmail.com>
2021-01-24 08:55:12 +0100

    Add button to redirect to searchscreen when favlist is empty

--- app/components/users/FavSection.js
+++ app/components/users/FavSection.js
@@ -1,21 +1,54 @@
 import React from 'react';
-import { getMediaType } from '../../functions/helpers';
-import { ScrollingContent } from '../common/Layout';
+import { useNavigation } from '@react-navigation/native';
+
 import SectionTitle from '../common/SectionTitle';
 import ArtistsList from '../media/artists/ArtistsList';
 import TrackList from '../media/tracks/TrackList';
+import CustomButton from '../common/buttons/CustomButton';
+import { ScrollingContent } from '../common/Layout';
+
+import { getMediaType } from '../../functions/helpers';
+import { StyleSheet, Text, View } from 'react-native';
+
+const RedirectButton = ({ type }) => {
+  const navigation = useNavigation()
+  return (
+    <CustomButton 
+      text={'Rechercher un ' + type}
+      onPress={() => navigation.navigate('Search')} />
+  )
+}
+
+const EmptyList = ({ type }) => {
+  return (
+    <View style={styles.emptyListContainer}>
+      <Text style={styles.emptyListLabel}>Aucun favori.</Text>
+      <RedirectButton type={type} />
+    </View>
+  )
+}
 
 export default ({ favorites }) => {
+  
   const tracksAndAlbums = favorites.filter(fav => getMediaType(fav) !== 'artist')
   const artists = favorites.filter(fav => getMediaType(fav) === 'artist')
 
   return (
     <ScrollingContent>
       <SectionTitle text='Morceaux et albums'/>
-      <TrackList showArtist tracks={tracksAndAlbums} />
+      {tracksAndAlbums.length ? <TrackList showArtist tracks={tracksAndAlbums} /> : <EmptyList type='média' />}
       <SectionTitle text='Artistes' />
-      <ArtistsList artists={artists} />
+      {artists.length ? <ArtistsList artists={artists} /> : <EmptyList type='artiste' />}
     </ScrollingContent>
   )
-
 }
+
+const styles = StyleSheet.create({
+  emptyListContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyListLabel: {
+    marginBottom: 10
+  }
+})
